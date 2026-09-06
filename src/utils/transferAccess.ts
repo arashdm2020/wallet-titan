e7c7d3f371cf485f19034c042af066bea2981dfd
Arash <arashdm2020@users.noreply.github.com>
2026-09-06 18:25:40 +0330

    Remove Iran time label from transfer timestamps

--- src/utils/transferAccess.ts
+++ src/utils/transferAccess.ts
@@ -3,7 +3,7 @@ import type { TransferAccess } from "@/domain/wallet";
 export function formatIranTime(value: string) {
   return new Intl.DateTimeFormat("en-GB", {
     timeZone: "Asia/Tehran", dateStyle: "medium", timeStyle: "medium", hourCycle: "h23",
-  }).format(new Date(value)) + " (Iran time, UTC+03:30)";
+  }).format(new Date(value)) + " (UTC+03:30)";
 }
 
 export function isTransferBlocked(access: TransferAccess | null, now: number) {
